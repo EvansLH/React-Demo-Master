ad883ac673aa6c6cb9cd2bd2297b06ec734f6d68
LH <liu.hong@ruanyun.net>
2017-07-13 10:17:31 +0800

    frist commit

---
+++ demo01/components/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import style from './App.css';
+import global from './global.css';
+
+
+export default () => {
+    return ( < h1 className = { global.title } > Hello World < /h1>);
+    };
